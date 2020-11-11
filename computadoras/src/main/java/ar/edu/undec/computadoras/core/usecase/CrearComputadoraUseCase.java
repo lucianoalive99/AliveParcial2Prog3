c74d1577dea6d2c37051c67dc5d70ad9b24fcf29
Luciano Alive <lucianoalive99@gmail.com>
2020-11-11 12:06:16 -0300

    paso del test de caso de uso

--- computadoras/src/main/java/ar/edu/undec/computadoras/core/usecase/CrearComputadoraUseCase.java
+++ computadoras/src/main/java/ar/edu/undec/computadoras/core/usecase/CrearComputadoraUseCase.java
@@ -1,12 +1,23 @@
 package ar.edu.undec.computadoras.core.usecase;
 
+import ar.edu.undec.computadoras.core.dominio.Computadora;
+import ar.edu.undec.computadoras.core.exception.ComputadoraExisteException;
 import ar.edu.undec.computadoras.core.repositorio.IComputadoraRepositorio;
+import net.bytebuddy.implementation.bytecode.Throw;
 
 public class CrearComputadoraUseCase {
 
-    private IComputadoraRepositorio computadoraRepositorio;
+    private final IComputadoraRepositorio computadoraRepositorio;
 
     public CrearComputadoraUseCase(IComputadoraRepositorio computadoraRepositorio) {
         this.computadoraRepositorio=computadoraRepositorio;
+    }
+
+    public boolean crearComputadora(Computadora laComputadora) throws ComputadoraExisteException {
+        if(computadoraRepositorio.existeComputadora(laComputadora.getResumen())){
+            throw new ComputadoraExisteException();
+        }else {
+            return computadoraRepositorio.guardarComputadora(laComputadora);
+        }
     }
 }
